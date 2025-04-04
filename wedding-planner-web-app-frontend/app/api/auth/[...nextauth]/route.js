b7dff7da837c61373f9b9425e6e450c9b230f762
Pranavtej1404 <113827801+Pranavtej1404@users.noreply.github.com>
2025-04-04 13:48:42 +0530

    Update route.js

--- wedding-planner-web-app-frontend/app/api/auth/[...nextauth]/route.js
+++ wedding-planner-web-app-frontend/app/api/auth/[...nextauth]/route.js
@@ -5,8 +5,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: '1038342948777-sesjs7v54t47lnt3bq7592d5r6gr9gea.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientId: '',
+      clientSecret: '',
     }),
     
   ],
